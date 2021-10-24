dafe258d1e75ae0a23fb7378997ba5fae459d40c
jowelahmedhim <jowelahmedhim@gmail.com>
2021-10-24 23:29:16 +0600

    Select Video File and get Uri,filename,path

--- app/src/main/java/com/example/masterchef/ui/UploadVideo.java
+++ app/src/main/java/com/example/masterchef/ui/UploadVideo.java
@@ -1,30 +1,57 @@
 package com.example.masterchef.ui;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.masterchef.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UploadVideo extends AppCompatActivity {
+public class UploadVideo extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    public static final int VIDEO_REQUEST_CODE = 100;
     Toolbar toolbar;
-    private EditText videoNameEt;
+    private EditText videoNameEt,videoDescriptionEt;
     private Spinner spinner ;
-    private TextView imageTv,videoTv,videoCategoryEt;
-    private Button upload;
+    private TextView imageTv,videoTv;
+    private Button upload,videoBtn;
+
+    Uri videoUri;
+    String videoCategory;
+    String videoTitle;
+    String currentUid;
+    StorageReference mStorageReference;
+    StorageTask mUploadTask;
+    DatabaseReference referenceVideos;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +71,12 @@ public class UploadVideo extends AppCompatActivity {
 
         init();
         spinnerFunction();
+        videoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openVideoFile();
+            }
+        });
 
 
 
@@ -52,11 +85,13 @@ public class UploadVideo extends AppCompatActivity {
 
     public void init(){
         videoNameEt = findViewById(R.id.video_title);
-        videoCategoryEt = findViewById(R.id.video_category);
+        videoDescriptionEt = findViewById(R.id.video_description);
+        spinner = findViewById(R.id.spinner);
         videoTv = findViewById(R.id.video_tv);
         imageTv = findViewById(R.id.image_tv);
         upload = findViewById(R.id.upload);
-        spinner = findViewById(R.id.spinner);
+        videoBtn = findViewById(R.id.vide_btn);
+
 
     }
 
@@ -73,6 +108,63 @@ public class UploadVideo extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_expandable_list_item_1);
         spinner.setAdapter(adapter);
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        videoCategory = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        videoCategory = adapterView.getItemAtPosition(0).toString();
+    }
+
+    public void openVideoFile(){
+        
+        startActivityForResult(Intent.createChooser(
+                new Intent(Intent.ACTION_GET_CONTENT)
+                        .setType("video/*"), "Choose Video"),VIDEO_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            switch (requestCode){
+                case VIDEO_REQUEST_CODE:
+                    if (data.getData() != null)
+                    {
+
+                        //get video uri , realPath, fileName
+                        videoUri = data.getData();
+                        String realPath = videoUri.getPath();
+                        Toast.makeText(getApplicationContext(), ""+realPath, Toast.LENGTH_SHORT).show();
+
+                        String[] projection = {MediaStore.MediaColumns.DISPLAY_NAME};
+                        ContentResolver cr = getApplicationContext().getContentResolver();
+                        Cursor metaCursor = cr.query(videoUri, projection, null, null, null);
+                        if (metaCursor != null) {
+                            try {
+                                if (metaCursor.moveToFirst()) {
+                                   String fileName = metaCursor.getString(0);
+                                    Toast.makeText(getApplicationContext(), ""+fileName, Toast.LENGTH_SHORT).show();
+                                    videoTv.setText(fileName);
+                                }
+                            } finally {
+                                metaCursor.close();
+                            }
+                        }
+
+                    }
+                    break;
+
+            }
+        }
+    }
+
 
 
 

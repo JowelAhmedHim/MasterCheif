47453e5951d263a79018941e189c8ba2f761978e
SaeedZ Pc <saeedofficial921@gmail.com>
2021-11-10 04:34:19 +0600

    Addedd Header Icon & Theme

--- app/src/main/java/com/example/masterchef/ui/activities/SettingActivity.java
+++ app/src/main/java/com/example/masterchef/ui/activities/SettingActivity.java
@@ -3,7 +3,9 @@ package com.example.masterchef.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import com.example.masterchef.R;
@@ -12,6 +14,13 @@ public class SettingActivity extends AppCompatActivity {
 Switch aSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+
+            setTheme(R.style.DarkTheme);
+        }else{
+
+            setTheme(R.style.Theme);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         aSwitch=findViewById(R.id.mode);
@@ -19,5 +28,28 @@ Switch aSwitch;
 
            aSwitch.setChecked(true);
        }
+       aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               if(isChecked){
+
+                   AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//reset();
+               }else{
+
+AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                  // reset();
+               }
+           }
+       });
     }
+
+    /*private void reset() {
+Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+startActivity(intent);
+finish();
+
+
+
+    }*/
 }
